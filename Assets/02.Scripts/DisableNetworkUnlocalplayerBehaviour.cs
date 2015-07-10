4327ab0d64663080e7d31577eacc1b95b829ad56
ishikura1 <ishikuratakuya00@gmail.com>
2015-07-10 12:16:15 +0900

    ネットワーク作成

---
+++ Assets/02.Scripts/DisableNetworkUnlocalplayerBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableNetworkUnlocalplayerBehaviour : NetworkBehaviour {
+	/*
+	[SerializeField]
+	Behaviour[] behaviours;
+	*/
+
+	public Behaviour cntrlScript;
+	
+	public Camera camera;
+
+	void Start()
+	{
+		if(!isLocalPlayer)
+		{
+			/*
+			foreach(var behaviour in behaviours)
+			{
+				behaviour.enabled = false;
+			}*/
+
+			cntrlScript.enabled = false;
+			
+			camera.enabled = false;
+		}
+	}
+	
+	void OnApplicationFocus(bool focusStatus)
+	{
+		if(isLocalPlayer)
+		{
+			/*
+			foreach(var behaviour in behaviours)
+			{
+				behaviour.enabled = focusStatus;	
+			}*/	
+			cntrlScript.enabled = focusStatus;
+			
+			camera.enabled = true;
+		}
+	}
+}
