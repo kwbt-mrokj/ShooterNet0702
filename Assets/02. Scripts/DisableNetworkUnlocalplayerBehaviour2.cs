28bff25cb122df6e56c6635bb2d3e09f38434c7d
kawabata <kwbt.6666@gmail.com>
2015-07-16 11:25:10 +0900

    UNETを使ったサーバーとクライアント処理

---
+++ Assets/02. Scripts/DisableNetworkUnlocalplayerBehaviour2.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableNetworkUnlocalplayerBehaviour2 : NetworkBehaviour
+{
+	public Behaviour cntrlScript;
+	public Camera camera;
+
+	void Start ()
+	{
+		if(! isLocalPlayer ){
+
+			cntrlScript.enabled = false;
+			camera.enabled = false;
+		}
+	}
+	void OnApplicationFocus(bool focusStatus) {
+		if(isLocalPlayer)
+		{
+			cntrlScript.enabled = focusStatus;
+			camera.enabled = true;
+		}
+	}
+}
